ef9ca38fbfa0d035db502d937288a10da8df8758
SharathMengani <chanduvarma57@gmail.com>
2026-02-20 19:31:06 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import Product from "./models/Product.js"
+import connectDB from "../config/db.js"
+import Product from "../models/Product.js"
 
-dotenv.config()
-connectDB()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Connect DB for every request (serverless safe)
+app.use(async (req, res, next) => {
+  await connectDB()
+  next()
+})
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({ message: "API Working 🚀" })
+})
 // -------------------- GET ALL PRODUCTS --------------------
 app.get("/products", async (req, res) => {
   try {
